c66a46bac08915435e754caece119230fde8a6bc
evergood <kanzann@gmail.com>
2020-04-02 21:41:44 +0300

    changes

--- src/main/java/com/egar/store/util/XmlParser.java
+++ src/main/java/com/egar/store/util/XmlParser.java
@@ -1,7 +1,6 @@
 package com.egar.store.util;
 
 import com.egar.store.domen.ItemPack;
-import lombok.extern.slf4j.Slf4j;
 import org.jibx.runtime.BindingDirectory;
 import org.jibx.runtime.IBindingFactory;
 import org.jibx.runtime.IUnmarshallingContext;
@@ -9,7 +8,6 @@ import org.jibx.runtime.IUnmarshallingContext;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-@Slf4j
 public class XmlParser {
 
     public ItemPack parseItems(String itemsFileName) throws Exception {
@@ -17,7 +15,6 @@ public class XmlParser {
         IUnmarshallingContext uctx = bfact.createUnmarshallingContext();
         ItemPack itemPack = (ItemPack) uctx.unmarshalDocument(
                 this.getClass().getClassLoader().getResourceAsStream(itemsFileName), null);
-        log.debug(itemPack.toString());
         return itemPack;
     }
 }
